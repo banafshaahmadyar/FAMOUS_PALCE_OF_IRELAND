76427b6222cb0814e44535b01a5bc8f7be5855f3
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2022-09-13 10:38:08 +0000

    add all information and dscription of five place in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+const city=[
+              {
+        id: 1,
+        name: "Dublin",
+        dscription:"Welcome to Dublin, a city that's as intimate as a village and as friendly as an Irish pub. Framed by mountains, centred on a river and edged by a beautiful bay, the city's streets and alleys are filled with vibrant art and historic buildings, hip cafés and traditional old man pubs, as Dubliners call them. Walk the streets and you'll feel the energy of over 1,000 years of history, as echoes of the Vikings mix with buzzing boutiques, cobbled streets reverberate with the sounds of buskers, and 18th century parks play host to festivals, film and food markets.",
+        src: "images/dublin 1.jpg"
+    },
+
+    {
+        id: 2,
+        name: "CORK",
+        description: "One of the best places to visit in southern Ireland, Cork follows Dublin as the Republic of Ireland's second largest city. It's also on par with the capital for being highly cosmopolitan. Wander the historic streets to soak up cool vibes emanating from small cafés, eccentric shops, unique museums (that means you, Cork Butter Museum), and lurid art galleries.",
+        src: "images/cork 2.jpg"
+    },
+
+    {
+        id: 3,
+        name: "BELFAST",
+        description:"In times of old, Belfast was a location non grata. Once an industrial powerhouse (its shipyard produced the doomed Titanic), this capital of Northern Ireland became feared. Today, Belfast has become a big tourist draw.",
+        src: "images/belfast 3.jpg"
+    },
+
+    {
+        id: 4,
+        name: "ARAN ISLAND ",
+        description:"Want a truly Irish experience? Immerse yourself in life on the Aran Islands. A popular spot for day-trippers from County Galway (a ferry leaves from Rossaveal) and County Clare (ferries depart from Doolin), those who want to thoroughly explore these three unique islands opt to stay longer. Visitors with deeper pockets can opt for a 10-minute flight to the area.",
+        src: "images/Aran island 4.jpg"
+    },
+
+    {
+        id: 5,
+        name: "KILLARNEY",
+        description: "County Kerry (on the southwest corner of Ireland) is home to the much-visited Killarney. Those who come here are in search of something a metropolitan high street doesn't have on offer: serenity. Take a look around, and you'll be graced by lakes, waterfalls, and mountains. Miles of trails are calling, and only comfortable sneakers (or hiking boots) will get you to your destination.",
+        src: "images/killarney 5.jpg"
+    }
+
+
+]
