a6cbd6557649f8231348c099685a56c277607a6b
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2022-09-23 07:05:10 +0000

    add content on html and js code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-const places=[
-              {
+const places = [
+    {
         id: 1,
         name: "Dublin",
-        dscription:"Welcome to Dublin, a city that's as intimate as a village and as friendly as an Irish pub. Framed by mountains, centred on a river and edged by a beautiful bay, the city's streets and alleys are filled with vibrant art and historic buildings, hip cafés and traditional old man pubs, as Dubliners call them. Walk the streets and you'll feel the energy of over 1,000 years of history, as echoes of the Vikings mix with buzzing boutiques, cobbled streets reverberate with the sounds of buskers, and 18th century parks play host to festivals, film and food markets.",
+        dscription: "Welcome to Dublin, a city that's as intimate as a village and as friendly as an Irish pub. Framed by mountains, centred on a river and edged by a beautiful bay, the city's streets and alleys are filled with vibrant art and historic buildings, hip cafés and traditional old man pubs, as Dubliners call them. Walk the streets and you'll feel the energy of over 1,000 years of history, as echoes of the Vikings mix with buzzing boutiques, cobbled streets reverberate with the sounds of buskers, and 18th century parks play host to festivals, film and food markets.",
         src: "assets/image/dublin 1.jpg"
     },
 
@@ -16,14 +16,14 @@ const places=[
     {
         id: 3,
         name: "BELFAST",
-        description:"In times of old, Belfast was a location non grata. Once an industrial powerhouse (its shipyard produced the doomed Titanic), this capital of Northern Ireland became feared. Today, Belfast has become a big tourist draw.",
+        description: "In times of old, Belfast was a location non grata. Once an industrial powerhouse (its shipyard produced the doomed Titanic), this capital of Northern Ireland became feared. Today, Belfast has become a big tourist draw.",
         src: "assets/image/belfast 3.jpg"
     },
 
     {
         id: 4,
         name: "ARAN ISLAND ",
-        description:"Want a truly Irish experience? Immerse yourself in life on the Aran Islands. A popular spot for day-trippers from County Galway (a ferry leaves from Rossaveal) and County Clare (ferries depart from Doolin), those who want to thoroughly explore these three unique islands opt to stay longer. Visitors with deeper pockets can opt for a 10-minute flight to the area.",
+        description: "Want a truly Irish experience? Immerse yourself in life on the Aran Islands. A popular spot for day-trippers from County Galway (a ferry leaves from Rossaveal) and County Clare (ferries depart from Doolin), those who want to thoroughly explore these three unique islands opt to stay longer. Visitors with deeper pockets can opt for a 10-minute flight to the area.",
         src: "assets/image/Aran islands 4.jpg"
     },
 
@@ -37,32 +37,20 @@ const places=[
 
 ]
 
-const imgs =document.querySelectorAll(".all-imgs img");
-const currentImages =document.querySelectorAll(".current-img");
-const titleImages =document.querySelectorAll(".image-title");
-const descImages =document.querySelectorAll(".image-desc");
+const images = document.getElementsByClassName("imgs");
+const currentImage = document.getElementById("current-img");
+const titleImages = document.getElementById("image-title");
+const descImages = document.getElementById("image-desc");
 
-console.log(imgs);
+for (let i = 0; i <= images.length; i++) {
+    images[i].setAttribute('src', places[i].src);
+    images[i].addEventListener("click", function () {
+        currentImage.setAttribute('src', places[i].src);
+        titleImages.innerText = places[i].name;
+        descImages.innerText = places[i].description;
+    });
+}
 
-imgs.forEach((image,index)=>
-{image.src=places[index].src;
-
- image.addEventListener("clik" ,function(){
-    currentImages.src=place[index].src;
-
- titleImage.innerText=places[index].name;
- descImage.innerText=places[index].description;
-
- currentImages.classList.add(".animate-imgs ")
-
- setTimeout(function(){
-    currentImages.classList.remove(".animate-imgs")
- })
-
-
- })  
-})
-
-currentImages.src=places[0].src;
-titleImages.innerText=places[0].name;
-descImages.innerText=places[0].description;
+currentImages.src = places[0].src;
+titleImages.innerText = places[0].name;
+descImages.innerText = places[0].description;
